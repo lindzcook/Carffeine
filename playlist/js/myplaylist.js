99cb455a46786cb488f1a205cfa9a85c1ad61eb4
Lindsey Cook <lindzcook@gmail.com>
2013-09-10 16:45:35 -0400

    First commit

---
+++ playlist/js/myplaylist.js
@@ -0,0 +1,92 @@
+
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/welcomevoice.mp3',
+        oga:'mix/1.ogg',
+        title:'Welcome',
+      
+    },
+    {
+        mp3:'mix/harrypotterg6.mp3',
+        oga:'mix/2.ogg',
+        title:'Like it\'s Quiddith',
+    },
+    {
+        mp3:'mix/Armageddon.mp3',
+        oga:'mix/3.ogg',
+        title:'Armageddon Speach',
+    },
+	  
+	  {
+        mp3:'mix/songsvoice.mp3',
+        oga:'mix/4.ogg',
+        title:'Tip songs',
+    },
+	  
+	  {
+        mp3:'mix/gangumstyle.mp3',
+        oga:'mix/5.ogg',
+        title:'Gangum Style',
+    },
+	 {
+        mp3:'mix/beatit.mp3',
+        oga:'mix/6.ogg',
+        title:'Beat It',
+    },
+	  
+	{
+        mp3:'mix/comedyvoice.mp3',
+        oga:'mix/7.ogg',
+        title:'Tip Comedy',
+    },
+	{
+        mp3:'mix/Superbad.mp3',
+        oga:'mix/8.ogg',
+        title:'Superbad',
+    },
+	{
+        mp3:'mix/theofficetoby.mp3',
+        oga:'mix/9.ogg',
+        title:'The Office',
+    },
+	{
+        mp3:'mix/Trampire.mp3',
+        oga:'mix/10.ogg',
+        title:'Trampire',
+    },
+	{
+        mp3:'mix/quizvoice.mp3',
+        oga:'mix/11.ogg',
+        title:'Tip DisneyQuiz',
+    },
+	{
+        mp3:'mix/Disneyquiz.mp3',
+        oga:'mix/12.ogg',
+        title:'Disney Quiz',
+    },
+	{
+        mp3:'mix/snacksvoice.mp3',
+        oga:'mix/13.ogg',
+        title:'Tip snacks',
+    },
+	{
+        mp3:'mix/firealarm.mp3',
+        oga:'mix/14.ogg',
+        title:'Fire Alarm',
+    },
+	{
+        mp3:'mix/rainbow.mp3',
+        oga:'mix/15.ogg',
+        title:'Rainbow',
+    },
+		{
+        mp3:'mix/rapist.mp3',
+        oga:'mix/16.ogg',
+        title:'Bed Intruder',
+    },
+	
+	
+];
+
